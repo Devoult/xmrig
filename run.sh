340a936629b499397431ee90cc8598a24375cb42
Devoult <running3bos@gmail.com>
2024-10-25 17:11:24 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo ./xmrig --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHsAcoQchEddkygrE3iW2JtQy7rTQmiPCAfmrBRGuU2QWzpMMb59YKBe6RUBJWJNHJZxnCvJWre4P3viL9uq8qjVkMusfXVy9 -p x -a rx/0 -k 
